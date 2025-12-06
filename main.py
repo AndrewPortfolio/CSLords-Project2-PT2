e0f2e8c21f153ff3af87e6e20224e572eb1ead1f
williamEkim <dongyeon0308@gmail.com>
2025-12-05 16:20:35 -0800

    updated normalization with and without option

--- main.py
+++ main.py
@@ -61,7 +61,6 @@ def normalize(dataset):
 if __name__ == "__main__":
     # test small dataset
     s_dataset = load_dataset("data/small-test-dataset-2-2.txt")
-    s_dataset = normalize(s_dataset)
 
     classifier = NNClassifier()
     validator = Validator()
@@ -76,7 +75,6 @@ if __name__ == "__main__":
 
     # test large dataset
     l_dataset = load_dataset("data/large-test-dataset-2.txt")
-    # l_dataset = normalize(l_dataset)
 
     #using [1, 15, 27] but since index starts from 0, it is rather [0, 14, 26]
     feature_subset = [0, 14, 26]
@@ -87,20 +85,42 @@ if __name__ == "__main__":
     print(f"Accuracy using features [1, 15, 27]: {round(accuracy*100,2)}% -- took {round(end-start, 5)}seconds")
 
 
+    # after normalization
+    print("\nApplying Normalization to each dataset... ", end='')
+    s_dataset = normalize(s_dataset)
+    l_dataset = normalize(l_dataset)
+    print("Done!")
+
+    feature_subset = [2, 4, 6]
+    start = time.time()
+    accuracy = validator.evaluate(s_dataset, classifier, feature_subset)
+    end = time.time()
+    print(f"Accuracy using features [3, 5, 7]: {round(accuracy*100,2)}% -- took {round(end-start, 5)}seconds")
+
+    feature_subset = [0, 14, 26]
+    start = time.time()
+    accuracy = validator.evaluate(l_dataset, classifier, feature_subset)
+    end = time.time()
+
+    print(f"Accuracy using features [1, 15, 27]: {round(accuracy*100,2)}% -- took {round(end-start, 5)}seconds")
+
+
+
+
     # ADDED TWO RANDOM TEST CASES TO SEE HOW IT WORKS!!
 
     #using [5, 20, 31] but since index starts from 0, it is rather [4, 19, 30]
-    feature_subset = [4, 19, 30]
-    start = time.time()
-    accuracy = validator.evaluate(l_dataset, classifier, feature_subset)
-    end = time.time()
+    # feature_subset = [4, 19, 30]
+    # start = time.time()
+    # accuracy = validator.evaluate(l_dataset, classifier, feature_subset)
+    # end = time.time()
 
-    print(f"Accuracy using features [5, 20, 31]: {round(accuracy*100,2)}% -- took {round(end-start, 5)}seconds")
+    # print(f"Accuracy using features [5, 20, 31]: {round(accuracy*100,2)}% -- took {round(end-start, 5)}seconds")
     
-    #using [10, 25, 36] but since index starts from 0, it is rather [9, 24, 35]
-    feature_subset = [9, 24, 35]
-    start = time.time()
-    accuracy = validator.evaluate(l_dataset, classifier, feature_subset)
-    end = time.time()
+    # #using [10, 25, 36] but since index starts from 0, it is rather [9, 24, 35]
+    # feature_subset = [9, 24, 35]
+    # start = time.time()
+    # accuracy = validator.evaluate(l_dataset, classifier, feature_subset)
+    # end = time.time()
 
-    print(f"Accuracy using features [10, 25, 36]: {round(accuracy*100,2)}% -- took {round(end-start, 5)}seconds")
+    # print(f"Accuracy using features [10, 25, 36]: {round(accuracy*100,2)}% -- took {round(end-start, 5)}seconds")
